96d6a4c523807174ced6f5f45201db33fa24b425
duke7able <mehtashreyan041.sm@gmail.com>
2017-02-08 12:09:39 +0530

    updated

--- app/src/main/java/ga/rdxgeek/de/MainActivity.java
+++ app/src/main/java/ga/rdxgeek/de/MainActivity.java
@@ -1,19 +1,37 @@
 package ga.rdxgeek.de;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(1000);  //Delay of 1 second
+                } catch (Exception e) {
+
+                } finally {
 
 
-        /*set and initialize variables*/
-
-
+                    Intent i = new Intent(SplashActivity.this,
+                            MainActivity.class);
+                    //asked for help on Stackoverflow..
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
     }
 
     @Override
---
+++ app/src/main/java/ga/rdxgeek/de/SplashActivity.java
@@ -0,0 +1,8 @@
+package ga.rdxgeek.de;
+
+/**
+ * Created by Shreyan on 2/8/2017.
+ */
+
+public class SplashActivity {
+}
