b9c79e34266b775220d361306a55dc580fff5079
duke7able <mehtashreyan041.sm@gmail.com>
2017-02-08 11:23:44 +0530

    Initial commit

---
+++ app/src/main/java/ga/rdxgeek/de/MainActivity.java
@@ -0,0 +1,34 @@
+package ga.rdxgeek.de;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*set and initialize variables*/
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+    }
+
+
+}
