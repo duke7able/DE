5130a35f4f0c3b69e3df4dc0a9a856a770e576d5
duke7able <mehtashreyan041.sm@gmail.com>
2017-04-14 18:11:01 +0530

    signup acitvity new push

--- app/src/main/java/ga/rdxgeek/de/signup.java
+++ app/src/main/java/ga/rdxgeek/de/signup.java
@@ -1,13 +1,171 @@
 package ga.rdxgeek.de;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class signup extends AppCompatActivity {
+
+    private LoginActivity.UserLoginTask mAuthTask = null;
+
+    private EditText mName;
+    private AutoCompleteTextView mEmail;
+    private EditText mFriendName;
+    private EditText mFriendMobile;
+    private EditText mPass;
+
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mName = (EditText) findViewById(R.id.name_input_signup);
+        mEmail = (AutoCompleteTextView) findViewById(R.id.email_signup);
+        mFriendName = (EditText) findViewById(R.id.email);
+        mFriendMobile = (EditText) findViewById(R.id.email);
+
+
+        mPass = (EditText) findViewById(R.id.email);
+
+        mPass.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptsignup();
+                    return true;
+                }
+                return false;
+            }
+        });
+        Button mNext = (Button) findViewById(R.id.next);
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptsignup();
+            }
+        });
+
+    }
+
+    private void attemptsignup(){
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmail.setError(null);
+        mPass.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmail.getText().toString();
+        String password = mPass.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPass.setError(getString(R.string.error_invalid_password));
+            focusView = mPass;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmail.setError(getString(R.string.error_field_required));
+            focusView = mEmail;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmail.setError(getString(R.string.error_invalid_email));
+            focusView = mEmail;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            ///showProgress(true);
+            //mAuthTask = new LoginActivity.UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            /*showProgress(false);*/
+
+            if (success) {
+                finish();
+            } else {
+                mPass.setError(getString(R.string.error_incorrect_password));
+                mPass.requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            //showProgress(false);
+        }
     }
 }
